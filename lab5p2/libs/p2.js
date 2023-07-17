01e5dccce4c28ff21248f6d0cefa29fad14062dc
dewmonk <natthaphong.t@kkumail.com>
2023-07-18 00:23:19 +0700

    add lab5 p2

---
+++ lab5p2/libs/p2.js
@@ -0,0 +1,28 @@
+function readInput() {
+    let lists = [];
+    let input;
+    
+    do {
+        input = prompt("Enter a positive integer number:");1
+
+        if (input && !isNaN(input) && parseInt(input) > 0) {
+            lists.push(parseInt(input));
+        }
+    } while (parseInt(input) >= 0 || isNaN(input));
+
+    return lists;
+}
+
+function displayStats(list) {
+    let answer = list.length > 0 ? list : "that is empty";
+    let sum = list.length > 0 ? list.reduce((a, b) => a + b, 0) : 0;
+    let average = list.length > 0 ? (sum / list.length).toFixed(2) : 0;
+    let min = list.length > 0 ? Math.min(...list) : 0;
+    let max = list.length > 0 ? Math.max(...list) : 0;
+
+    alert("For the list " + answer + " ,the average is " + average + " ,the minimum is " + min + " ,and the maximum is " + max);
+}
+
+
+list = readInput();
+displayStats(list);
